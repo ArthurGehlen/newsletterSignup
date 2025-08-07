8d51ab8649a00642d8a6c91e5dbd3d9772e22878
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-08-06 23:07:52 -0300

    Finalizando o site.

--- src/App.jsx
+++ src/App.jsx
@@ -12,74 +12,78 @@ const check_texts = [
 function App() {
   const [email, setEmail] = useState("");
   const [emailError, setEmailError] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
 
-  const check_email_errors = () => {
-    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (email === "" || !regex.test(email)) {
-      setEmailError(true);
-      return;
-    }
-    else {
-      setEmailError(false);
-    }
+  const isValidEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
   };
 
   const handle_submit = (e) => {
     e.preventDefault();
 
-    check_email_errors();
+    if (!isValidEmail(email)) {
+      setEmailError(true);
+      return;
+    }
 
-    document.getElementById("main").style.display = "none";
-    document.getElementById("response").style.display = "flex";
+    setEmailError(false);
+    setSubmitted(true);
   };
 
   const handle_dismiss = () => {
-    document.getElementById("main").style.display = "flex";
-    document.getElementById("response").style.display = "none";
-  }
+    setSubmitted(false);
+    setEmail("");
+  };
 
   return (
     <>
-      <main id="main">
-        <section className="image_section"></section>
-        <section className="infos_section">
-          <h1>Stay updated!</h1>
-          <p>Join 60,000+ product managers receiving monthly updates on:</p>
-          <div className="checkers_container">
-            {check_texts.map((content, index) => [
-              <CheckInfo key={index} text={content} />,
-            ])}
-          </div>
-          <form onSubmit={handle_submit}>
-            <div className="input_container">
-              <div className="label_container">
-                <label htmlFor="email">Email address</label>
-                {emailError && 
-                  <label className="error_label" htmlFor="email">Valid email required</label>
-                }
-              </div>
-              <input
-                className={emailError ? "input_error" : ""}
-                type="text"
-                name="email"
-                placeholder="email@company.com"
-                onChange={(e) => setEmail(e.target.value)}
-              />
+      {!submitted ? (
+        <main id="main">
+          <section className="image_section"></section>
+          <section className="infos_section">
+            <h1>Stay updated!</h1>
+            <p>Join 60,000+ product managers receiving monthly updates on:</p>
+            <div className="checkers_container">
+              {check_texts.map((content, index) => (
+                <CheckInfo key={index} text={content} />
+              ))}
             </div>
-            <input type="submit" value="Subscribe to monthly newsletter" />
-          </form>
-        </section>
-      </main>
-
-      <div id="response">
-        <img src={icon_success} alt="Success" />
-        <h1>Thanks for subscribing!</h1>
-        <p>
-          A confirmation email has been sent to <strong>{email}</strong>. Please
-          open it and click the button inside to confirm your sbscription.
-        </p>
-        <button onClick={handle_dismiss}>Dismiss message</button>
-      </div>
+            <form onSubmit={handle_submit}>
+              <div className="input_container">
+                <div className="label_container">
+                  <label htmlFor="email">Email address</label>
+                  {emailError && (
+                    <label className="error_label" htmlFor="email">
+                      Valid email required
+                    </label>
+                  )}
+                </div>
+                <input
+                  className={emailError ? "input_error" : ""}
+                  type="text"
+                  name="email"
+                  placeholder="email@company.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <input type="submit" value="Subscribe to monthly newsletter" />
+            </form>
+          </section>
+        </main>
+      ) : (
+        <div id="response">
+          <img src={icon_success} alt="Success" />
+          <h1>Thanks for subscribing!</h1>
+          <p>
+            A confirmation email has been sent to <strong>{email}</strong>.
+            Please open it and click the button inside to confirm your
+            subscription.
+          </p>
+          <button onClick={handle_dismiss}>Dismiss message</button>
+        </div>
+      )}
     </>
   );
 }
